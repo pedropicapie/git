6e8e029b3c98b0f634c423e1836fb20d9b86266e
Adrian <Adrian@LAPTOP-172LSVQK>
2021-02-18 20:20:45 -0600

    repositorio de prueba

---
+++ src/git6360/Git6360.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git6360;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Git6360 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nombre = "adrian luna";
+       String matricula = "63960";
+       
+       System.out.println("soy " +nombre +matricula);
+    }
+    
+}
